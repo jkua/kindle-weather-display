40bd46065738f073213796dd9ec33d0e409df8ea
John Kua <john@kua.fm>
2025-05-01 18:53:38 -0700

    Updated for Python 3 and switch weather provider to Open-Meteo

--- server/weather-script.py
+++ server/weather-script.py
@@ -1,207 +1,213 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 # Kindle Weather Display
 # Originally by Matthew Petroff (http://www.mpetroff.net/)
 # Modified by John Kua (http://john.kua.fm)
-# March 2014
 
-import urllib2
-from xml.dom import minidom
+import urllib.request
+import urllib.error
+import json
 import datetime
 import codecs
 import sys
+import ephem
 
-# Get location from command line
-if len(sys.argv) >= 3:
-	latitude = sys.argv[1]
-	longitude = sys.argv[2]
-	if len(sys.argv) >= 4:
-		wundergroundApiKey = sys.argv[3]
-else:
-	latitude = 37
-	longitude = -122
+def getWebpage(url):
+	try:
+		response = urllib.request.urlopen(url)
+		webpage = response.read().decode('utf-8')
+	except urllib.error.URLError as e:
+		print(f"Error opening URL: {e}")
+	except Exception as e:
+		print(f"An unexpected error occurred: {e}")
 
-#
-# Download and parse weather data
-#
-
-useWunderground = True
-
-if useWunderground == True:
-	iconDict = dict()
-	iconDict['chanceflurries']='sn' #bad
-	iconDict['chancerain']='hi_shwrs'
-	iconDict['chancesleet']='ip' #bad
-	iconDict['chancesnow']='sn' #bad
-	iconDict['chancetstorms']='scttsra'
-	iconDict['clear']='skc'
-	iconDict['cloudy']='ovc'
-	iconDict['flurries']='sn' # not a good match
-	iconDict['fog']='fg'
-	iconDict['haze']='du' #eh
-	iconDict['mostlycloudy']='bkn'
-	iconDict['partlysunny']='bkn'
-	iconDict['mostlysunny']='sct'
-	iconDict['partlycloudy']='sct'
-	iconDict['rain']='ra'
-	iconDict['sleet']='ip'
-	iconDict['snow']='sn'
-	iconDict['tstorms']='tsra'
-	iconDict['unknown']='skc' #bad
+	return webpage
 
 
-	weatherXml = urllib2.urlopen('http://api.wunderground.com/api/' + wundergroundApiKey + '/astronomy/conditions/forecast10day/hourly10day/tide/yesterday/q/' + str(latitude) + ',' + str(longitude) + '.xml').read()
-	dom = minidom.parseString(weatherXml)
-	simpleForecastXml = dom.getElementsByTagName('simpleforecast')
-	forecastDayXml = simpleForecastXml[0].getElementsByTagName('forecastday')
-	highs = []
-	lows = []
-	icons = []
-	pop = []
-	qpf_day =[]
-	qpf_night = []
-	snow_day = []
-	snow_night = []
-	for forecastDay in forecastDayXml:
-		tempsXml = forecastDay.getElementsByTagName('fahrenheit')
-		highs.append(tempsXml[0].firstChild.nodeValue)
-		lows.append(tempsXml[1].firstChild.nodeValue)
-		iconXml = forecastDay.getElementsByTagName('icon')
-		icons.append(iconDict[iconXml[0].firstChild.nodeValue])
-		popXml = forecastDay.getElementsByTagName('pop')
-		pop.append(int(popXml[0].firstChild.nodeValue))
-		rainQuantityXml = forecastDay.getElementsByTagName('in')
-		qpf_day.append(float(rainQuantityXml[0].firstChild.nodeValue))
-		qpf_night.append(float(rainQuantityXml[1].firstChild.nodeValue))
-		snow_day.append(float(rainQuantityXml[2].firstChild.nodeValue))
-		snow_night.append(float(rainQuantityXml[3].firstChild.nodeValue))
-	day = int(forecastDayXml[0].getElementsByTagName('day')[0].firstChild.nodeValue)
-	month = int(forecastDayXml[0].getElementsByTagName('month')[0].firstChild.nodeValue)
-	year = int(forecastDayXml[0].getElementsByTagName('year')[0].firstChild.nodeValue)
-	day_one = datetime.date(year, month, day)
+class WeatherData:
+	def __init__(self):
+		self.latitude = None
+		self.longitude = None
+		self.timezone = None
+		self.temperature_unit = None
+		self.precipitation_unit = None
+		self.wind_speed_unit = None
 
-	txtForecastXml = dom.getElementsByTagName('txt_forecast')
-	txtForecastDayXml = txtForecastXml[0].getElementsByTagName('forecastday')
-	pop_day = []
-	pop_night = []
-	for i, forecastDay in enumerate(txtForecastDayXml):
-		popXml = forecastDay.getElementsByTagName('pop')
-		if i % 2 == 0:
-			pop_day.append(int(popXml[0].firstChild.nodeValue))
-		else:
-			pop_night.append(int(popXml[0].firstChild.nodeValue))
+		self.forecast_retrieval_time = None
+		self.forecast_source = ''
+		self.date = None
+		self.weather_code = None
+		self.temperature_high = None
+		self.temperature_low = None
+		self.temperature_current = None
+		self.precipitation_probability_max = None
+		self.precipitation_probability_day = None
+		self.precipitation_probability_night = None
+		self.wind_speed = None
+		self.wind_direction = None
+		self.sunrise = None
+		self.sunset = None
+		self.moon_phase = None
+		self.moon_age = None
 
-	currentObservationXml = dom.getElementsByTagName('current_observation')
-	currentTemperatureXml = currentObservationXml[0].getElementsByTagName('temp_f')
-	currentTemperature = str(int(round(float(currentTemperatureXml[0].firstChild.nodeValue))))
-	windSpeedXml = currentObservationXml[0].getElementsByTagName('wind_mph')
-	windSpeed = windSpeedXml[0].firstChild.nodeValue
-	windDirectionXml = currentObservationXml[0].getElementsByTagName('wind_dir')
-	windDirection = windDirectionXml[0].firstChild.nodeValue
+	def compute_moon_info(self):
+		if self.date is None or self.latitude is None or self.longitude is None:
+			return
 
-	moonPhaseXml = dom.getElementsByTagName('moon_phase')[0]
-	moonPercentIlluminated = moonPhaseXml.getElementsByTagName('percentIlluminated')[0].firstChild.nodeValue
-	moonAge = moonPhaseXml.getElementsByTagName('ageOfMoon')[0].firstChild.nodeValue
-	sunriseXml = moonPhaseXml.getElementsByTagName('sunrise')[0]
-	sunriseHour = sunriseXml.getElementsByTagName('hour')[0].firstChild.nodeValue
-	sunriseMinute = sunriseXml.getElementsByTagName('minute')[0].firstChild.nodeValue
-	sunsetXml = moonPhaseXml.getElementsByTagName('sunset')[0]
-	sunsetHour = sunsetXml.getElementsByTagName('hour')[0].firstChild.nodeValue
-	sunsetMinute = sunsetXml.getElementsByTagName('minute')[0].firstChild.nodeValue
-	sunrise = datetime.time(int(sunriseHour),int(sunriseMinute))
-	sunset = datetime.time(int(sunsetHour),int(sunsetMinute))
+		# Create observer object for the given date and location
+		observer = ephem.Observer()
+		observer.lat = str(self.latitude)
+		observer.lon = str(self.longitude)
+		observer.date = self.date
 
-else:
-	# Fetch data (change lat and lon to desired location)
-	weather_xml = urllib2.urlopen('http://graphical.weather.gov/xml/SOAP_server/ndfdSOAPclientByDay.php?whichClient=NDFDgenByDay&lat=' + latitude + '&lon=' + longitude + '&format=24+hourly&numDays=4&Unit=e').read()
-	dom = minidom.parseString(weather_xml)
+		# Get the moon phase and age
+		self.moon_phase = ephem.Moon(observer).phase
 
-	# Parse temperatures
-	xml_temperatures = dom.getElementsByTagName('temperature')
-	highs = [None]*4
-	lows = [None]*4
-	for item in xml_temperatures:
-		if item.getAttribute('type') == 'maximum':
-			values = item.getElementsByTagName('value')
-			for i in range(len(values)):
-				highs[i] = int(values[i].firstChild.nodeValue)
-		if item.getAttribute('type') == 'minimum':
-			values = item.getElementsByTagName('value')
-			for i in range(len(values)):
-				lows[i] = int(values[i].firstChild.nodeValue)
+		date_ephem = ephem.Date(self.date)
+		previous_new_moon = ephem.previous_new_moon(date_ephem)
+		self.moon_age = date_ephem - previous_new_moon
 
-	
-	# Parse icons
-	xml_icons = dom.getElementsByTagName('icon-link')
-	icons = [None]*4
-	for i in range(len(xml_icons)):
-		icons[i] = xml_icons[i].firstChild.nodeValue.split('/')[-1].split('.')[0].rstrip('0123456789')
+	@staticmethod
+	def get_from_open_meteo(latitude, longitude, timezone='America/Los_Angeles', temperature_unit='fahrenheit', precipitation_unit='inch', wind_speed_unit='mph'):
+		url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude:4f}&longitude={longitude:.4f}'
+		url += '&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max,weather_code,sunrise,sunset,wind_speed_10m_max,wind_direction_10m_dominant'
+		url += '&hourly=precipitation_probability,is_day'
+		url += '&current=temperature_2m'
+		url += f'&timezone={timezone}&wind_speed_unit={wind_speed_unit}&temperature_unit={temperature_unit}&precipitation_unit={precipitation_unit}'
 
-	# Parse dates
-	xml_day_one = dom.getElementsByTagName('start-valid-time')[0].firstChild.nodeValue[0:10]
-	day_one = datetime.datetime.strptime(xml_day_one, '%Y-%m-%d')
+		webpage = getWebpage(url)
+		response_data = json.loads(webpage)
+		forecast_retrieval_time = datetime.datetime.now()
+
+		# Compute day/night max precipitation probabilities
+		max_precipitation_probabilities_day = []
+		max_precipitation_probabilities_night = []
+
+		state = 'START'
+		for time_string, is_day, precipitation_probability in zip(response_data['hourly']['time'], response_data['hourly']['is_day'], response_data['hourly']['precipitation_probability']):
+			if state == 'START':
+				if not is_day:
+					continue
+				else:
+					state = 'DAY'
+					max_precipitation_probabilities_day.append(0)
+			elif state == 'DAY':
+				if is_day:
+					if precipitation_probability > max_precipitation_probabilities_day[-1]:
+						max_precipitation_probabilities_day[-1] = precipitation_probability
+				else:
+					state = 'NIGHT'
+					max_precipitation_probabilities_night.append(0)
+			elif state == 'NIGHT':
+				if not is_day:
+					if precipitation_probability > max_precipitation_probabilities_night[-1]:
+						max_precipitation_probabilities_night[-1] = precipitation_probability
+				else:
+					state = 'DAY'
+					max_precipitation_probabilities_day.append(0)
+
+		# Save daily forecasts
+		daily_forecasts = []
+		for i in range(len(response_data['daily']['time'])):
+			forecast = WeatherData()
+			forecast.latitude = latitude
+			forecast.longitude = longitude
+			forecast.timezone = timezone
+			forecast.temperature_unit = temperature_unit
+			forecast.precipitation_unit = precipitation_unit
+			forecast.wind_speed_unit = wind_speed_unit
+
+			forecast.forecast_retrieval_time = forecast_retrieval_time
+			forecast.forecast_source = 'Open-Meteo'
+			
+			if i == 0:
+				forecast.temperature_current = response_data['current']['temperature_2m']
+				
+			forecast.date = datetime.date.fromisoformat(response_data['daily']['time'][i])
+			forecast.weather_code = response_data['daily']['weather_code'][i]
+			forecast.temperature_high = response_data['daily']['temperature_2m_max'][i]
+			forecast.temperature_low = response_data['daily']['temperature_2m_min'][i]
+			forecast.precipitation_probability_max = response_data['daily']['precipitation_probability_max'][i]
+			forecast.precipitation_probability_day = max_precipitation_probabilities_day[i]
+			forecast.precipitation_probability_night = max_precipitation_probabilities_night[i]
+			forecast.sunrise = datetime.datetime.fromisoformat(response_data['daily']['sunrise'][i])
+			forecast.sunset = datetime.datetime.fromisoformat(response_data['daily']['sunset'][i])
+			forecast.wind_speed = response_data['daily']['wind_speed_10m_max'][i]
+			forecast.wind_direction = response_data['daily']['wind_direction_10m_dominant'][i]
+
+			forecast.compute_moon_info()
+
+			daily_forecasts.append(forecast)
+
+		return daily_forecasts
 
 
+def update_weather_svg(daily_forecasts):
+	icon_dict = {0: 'skc', 1: 'sct', 2: 'bkn', 3: 'ovc', 
+			    45: 'fg', 48: 'fg',
+			    51: 'ra', 53: 'ra', 55: 'ra',
+			    56: 'ip', 57: 'ip',
+			    61: 'ra', 63: 'ra', 65: 'ra',
+			    66: 'ip', 67: 'ip',
+			    71: 'sn', 73: 'sn', 75: 'sn',
+			    77: 'sn',
+			    80: 'ra', 81: 'ra', 82: 'ra',
+			    85: 'sn', 86: 'sn',
+			    95: 'tsra', 96: 'tsra', 99: 'tsra'}
 
-#
-# Preprocess SVG
-#
-
-# Open SVG to process
-if useWunderground == True:
-	output = codecs.open('weather-script-wunderground-preprocess.svg', 'r', encoding='utf-8').read()
-	output = output.replace('TEMP_NOW',currentTemperature)
-	output = output.replace('SUNRISE',sunriseHour + ':' + sunrise.strftime('%M'))
-	output = output.replace('SUNSET',str(sunset.hour-12) + ':' + sunset.strftime('%M'))
-	output = output.replace('MOON_ILLUMINATION',moonPercentIlluminated)
-	output = output.replace('MOON_AGE',moonAge)
-	output = output.replace('WIND_SPEED',windSpeed)
-	output = output.replace('WIND_DIR',windDirection)
-
-	if pop[0] > 10:
-		percentPrecipitation = '<g font-family="DejaVu Sans">\n'
-		percentPrecipitation += '<text style="text-anchor:end;" font-size="20px" y="300" x="380">Day: ' + str(pop_day[0]) +'%</text>\n'
-		percentPrecipitation += '<text style="text-anchor:end;" font-size="20px" y="325" x="380">Night: ' + str(pop_night[0]) +'%</text>\n'
-		percentPrecipitation += '</g>\n</svg>' 
-		output = output.replace('</svg>',percentPrecipitation)
-
-	if pop[1] > 10:
-		percentPrecipitation = '<g font-family="DejaVu Sans">\n'
-		percentPrecipitation += '<text style="text-anchor:end;" font-size="15px" y="580" x="180">' + str(pop[1]) +'%</text>\n'
-		percentPrecipitation += '</g>\n</svg>' 
-		output = output.replace('</svg>',percentPrecipitation)
-
-	if pop[2] > 10:
-		percentPrecipitation = '<g font-family="DejaVu Sans">\n'
-		percentPrecipitation += '<text style="text-anchor:end;" font-size="15px" y="580" x="380">' + str(pop[2]) +'%</text>\n'
-		percentPrecipitation += '</g>\n</svg>' 
-		output = output.replace('</svg>',percentPrecipitation)
-
-	if pop[3] > 10:
-		percentPrecipitation = '<g font-family="DejaVu Sans">\n'
-		percentPrecipitation += '<text style="text-anchor:end;" font-size="15px" y="580" x="580">' + str(pop[3]) +'%</text>\n'
-		percentPrecipitation += '</g>\n</svg>' 
-		output = output.replace('</svg>',percentPrecipitation)
-else:
+	# Open SVG to process
 	output = codecs.open('weather-script-preprocess.svg', 'r', encoding='utf-8').read()
+	output = output.replace('TEMP_NOW', f'{daily_forecasts[0].temperature_current:.0f}')
+	output = output.replace('SUNRISE', daily_forecasts[0].sunrise.strftime('%-I:%M'))
+	output = output.replace('SUNSET', daily_forecasts[0].sunset.strftime('%-I:%M'))
+	output = output.replace('MOON_ILLUMINATION', str(int(round(daily_forecasts[0].moon_phase))))
+	output = output.replace('MOON_AGE', str(int(daily_forecasts[0].moon_age)))
+	output = output.replace('WIND_SPEED', f'{daily_forecasts[0].wind_speed:.0f}')
+	output = output.replace('WIND_DIR', f'{daily_forecasts[0].wind_direction:0.0f}°')
 
-# Insert icons and temperatures
-output = output.replace('ICON_ONE',icons[0]).replace('ICON_TWO',icons[1]).replace('ICON_THREE',icons[2]).replace('ICON_FOUR',icons[3])
-output = output.replace('HIGH_ONE',str(highs[0])).replace('HIGH_TWO',str(highs[1])).replace('HIGH_THREE',str(highs[2])).replace('HIGH_FOUR',str(highs[3]))
-output = output.replace('LOW_ONE',str(lows[0])).replace('LOW_TWO',str(lows[1])).replace('LOW_THREE',str(lows[2])).replace('LOW_FOUR',str(lows[3]))
+	output = output.replace('WEATHER_SOURCE', daily_forecasts[0].forecast_source)
+	output = output.replace('DATE_STRING', daily_forecasts[0].forecast_retrieval_time.strftime('%H:%M %B %-d, %Y'))
 
-# Insert days of week
-one_day = datetime.timedelta(days=1)
-days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-now = datetime.datetime.now()
-if day_one.day > now.day:
-	output = output.replace('DAY_ONE','Tomorrow').replace('DAY_TWO',days_of_week[(day_one + one_day).weekday()])
-else:
-	output = output.replace('DAY_ONE','Today').replace('DAY_TWO','Tomorrow')
-output = output.replace('DAY_THREE',days_of_week[(day_one + 2*one_day).weekday()]).replace('DAY_FOUR',days_of_week[(day_one + 3*one_day).weekday()])
+	number_words = ['ONE', 'TWO', 'THREE', 'FOUR']
 
-# Insert processing time
-output = output.replace('DATE_STRING',now.strftime('%H:%M %B %d, %Y'))
+	for number_word, forecast in zip(number_words, daily_forecasts):
+		print(f'Processing forecast for {number_word}: {forecast.date} - High: {forecast.temperature_high}, Low: {forecast.temperature_low}, Weather Code: {forecast.weather_code}')
+		output = output.replace(f'ICON_{number_word}', icon_dict[forecast.weather_code])
+		output = output.replace(f'HIGH_{number_word}', f'{forecast.temperature_high:.0f}')
+		output = output.replace(f'LOW_{number_word}', f'{forecast.temperature_low:.0f}')
 
-# Write output
-codecs.open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
+		if number_word != 'ONE':
+			if number_word == 'TWO':
+				day_name = 'Tomorrow'
+			else:
+				day_name = forecast.date.strftime('%A')
+			output = output.replace(f'DAY_{number_word}', day_name)
+
+		if forecast.precipitation_probability_max > 10:
+			percent_precip_svg = '<g font-family="DejaVu Sans">\n'
+			if number_word == 'ONE':
+				percent_precip_svg += '<text style="text-anchor:end;" font-size="20px" y="300" x="380">Day: ' + str(forecast.precipitation_probability_day) +'%</text>\n'
+				percent_precip_svg += '<text style="text-anchor:end;" font-size="20px" y="325" x="380">Night: ' + str(forecast.precipitation_probability_night) +'%</text>\n'
+			elif number_word == 'TWO':
+				percent_precip_svg += '<text style="text-anchor:end;" font-size="15px" y="580" x="180">' + str(forecast.precipitation_probability_max) +'%</text>\n'
+			elif number_word == 'THREE':
+				percent_precip_svg += '<text style="text-anchor:end;" font-size="15px" y="580" x="380">' + str(forecast.precipitation_probability_max) +'%</text>\n'
+			elif number_word == 'FOUR':
+				percent_precip_svg += '<text style="text-anchor:end;" font-size="15px" y="580" x="580">' + str(forecast.precipitation_probability_max) +'%</text>\n'
+			percent_precip_svg += '</g>\n</svg>' 
+			output = output.replace('</svg>',percent_precip_svg)
+
+	codecs.open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
+
+
+if __name__=='__main__':
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('latitude', type=float, default=37.0, help='Latitude of the location (default: 37.0)')
+	parser.add_argument('longitude', type=float, default=-122.0, help='Longitude of the location (default: -122.0)')
+	parser.add_argument('timezone', type=str, default='America/Los_Angeles', help='Timezone of the location (default: America/Los_Angeles)')
+	args = parser.parse_args()
+
+	daily_forecasts = WeatherData.get_from_open_meteo(args.latitude, args.longitude, timezone=args.timezone)
+
+	update_weather_svg(daily_forecasts)
--- server/weather-script.sh
+++ server/weather-script.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"
+PATH=$PATH:/home/jkua/bin
+# Oakland
+python weather-script.py 37.811645 -122.264835 America/Los_Angeles
 
-python2 weather-script.py
-rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg
+cp -f weather-script-output.svg /home/jkua/john.kua.fm/kindle-weather/weather-script-output.svg
+cairosvg weather-script-output.svg -b white -o weather-script-output.png
 pngcrush -c 0 -ow weather-script-output.png
-cp -f weather-script-output.png /path/to/web/server/directory/weather-script-output.png
+cp -f weather-script-output.png /home/jkua/john.kua.fm/kindle-weather/weather-script-output.png
