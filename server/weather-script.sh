26f7e4a38cd33071aea10cf5d3b7efd269a5b089
John Kua <john@kua.fm>
2025-05-01 19:11:16 -0700

    Ensure Python 3 is used in script

--- server/weather-script.sh
+++ server/weather-script.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 PATH=$PATH:/home/jkua/bin
 # Oakland
-python weather-script.py 37.811645 -122.264835 America/Los_Angeles
+python3 weather-script.py 37.811645 -122.264835 America/Los_Angeles
 
 cp -f weather-script-output.svg /home/jkua/john.kua.fm/kindle-weather/weather-script-output.svg
 cairosvg weather-script-output.svg -b white -o weather-script-output.png
